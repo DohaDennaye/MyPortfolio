a55d7e571d1979375fc008e25e9b9ae492de09d6
DohaDennaye <dennayed@gmail.com>
2025-10-12 13:45:33 +0100

    Description de la mise à jour

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -58,7 +58,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Doha Dennaye`} <span className="txt-rotate" dataPeriod="1000" data-rotate='["Developpeuse web Full stack", "Full stack Developer", "Web Developer " ]'><span className="wrap">{text}</span></span></h1>
-                  <p>EN : Building digital experiences that blend creativity and technology — as a passionate Full Stack Web Developer, I design and develop modern, efficient, and user-focused applications that bring ideas to life.<br/>FR : Créer des expériences numériques qui unissent créativité et technologie — en tant que développeuse web Full Stack passionnée, je conçois et développe des applications modernes, performantes et centrées sur l’utilisateur, donnant vie aux idées.</p>
+                  <p>Building digital experiences that blend creativity and technology — as a passionate Full Stack Web Developer, I design and develop modern, efficient, and user-focused applications that bring ideas to life.</p>
                   <button  className="vvd" onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
